d27a3b0c3526550f636daf27b93a1a1aa57ca33e
Raghav Sejpal <sejpalraghav05@gmail.com>
2025-05-21 12:47:15 +0530

    Functionality of toggle button improved

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const toggleBtn = document.getElementById("theme-toggle");
 const lightIcon = document.getElementById("light-icon");
 const darkIcon = document.getElementById("dark-icon");
 
+const theme = document.documentElement.getAttribute("data-theme");
+
+if (theme === "dark") {
+    lightIcon.style.display = "none";
+    darkIcon.style.display = "inline";
+} else {
+    lightIcon.style.display = "inline";
+    darkIcon.style.display = "none";
+}
+
 toggleBtn.addEventListener("click", () => {
   const html = document.documentElement;
-  const currentTheme = html.getAttribute("data-theme") || "light";
+  const currentTheme = html.getAttribute("data-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
 
   html.setAttribute("data-theme", newTheme);
